ea81fd154581ecdf4b8b0ddf2c50ee727ed9aad4
Vanshii14 <82251741+Vanshii14@users.noreply.github.com>
2021-04-14 17:34:48 +0530

    Add files via upload

---
+++ Let's code loops(Task 2).py
@@ -0,0 +1,18 @@
+#Python program to print all positive nos.
+
+
+list1=[12,-7,5,64,-14]
+print("List 1=",list1)
+print("The positive numbers in List 1 are :")
+for num in list1:
+    if num >= 0:
+        print(num)
+
+
+
+list2=[12,14,-95,3]
+print("List 2=",list2)
+print("The positive numbers in List 2 are :")
+for num in list2:
+    if num >= 0:
+        print(num)
